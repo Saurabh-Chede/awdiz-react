98086ea542182ce1f0482795d659cfa9274d64b7
Saurabh-Chede <saurabhchede21@gmail.com>
2026-03-09 19:53:56 +0530

    react-day-2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 style={{color:"orangered"}}>React Learning</h1>
+      <p style={{marginTop:"20px"}}>JSX Practice</p>
+      <button>Start Learning</button>
+      <p>Saurabh chede</p>
+    </>
+  );
+}
